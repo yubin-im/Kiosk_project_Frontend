1b36b947698ba91d414c356249280a8b2ab6546e
jennyjoo <100cllr@gmail.com>
2024-05-03 14:45:58 +0900

    feat : 주문결제 화면 디자인 변경

--- src/components/view/OrderPayment.tsx
+++ src/components/view/OrderPayment.tsx
@@ -40,20 +40,22 @@ const OrderPayment = () => {
   }
 
   return (
-    <div>
-      <h1>주문을 확인하세요.</h1>
-      <br />
-      <p>
-        총 수량: {data.orderItemTotalAmount}개 총 가격:{' '}
-        {data.orderListTotalPrice}원
-      </p>
-      <br />
-      <p>카드를 화살표 방향으로 투입구에 넣어주세요.</p>
-      <p>결제 오류시, 카드를 긁어주세요.</p>
-      <img
-        src='https://media.licdn.com/dms/image/D4D12AQFym_70hWOyZg/article-cover_image-shrink_600_2000/0/1685446331482?e=2147483647&v=beta&t=fuITp9VITQaAdGNjomWZkykJGMIoho2qh2SIdDOD1eE'
-        alt='pay'
-      ></img>
+    <div className='flex flex-col max-w-screen-sm sm min-h-screen bg-mcred justify-between mx-auto p-20'>
+      <div className='flex flex-col gap-2 bg-white rounded-3xl px-2 py-10 pt-2'>
+        <h1 className='text-xl font-bold'>주문을 확인하세요.</h1>
+        <br />
+        <p>
+          총 수량: {data.orderItemTotalAmount}개 총 가격:{' '}
+          {data.orderListTotalPrice}원
+        </p>
+        <br />
+        <p>카드를 화살표 방향으로 투입구에 넣어주세요.</p>
+        <p>결제 오류시, 카드를 긁어주세요.</p>
+        <img
+          src='https://media.licdn.com/dms/image/D4D12AQFym_70hWOyZg/article-cover_image-shrink_600_2000/0/1685446331482?e=2147483647&v=beta&t=fuITp9VITQaAdGNjomWZkykJGMIoho2qh2SIdDOD1eE'
+          alt='pay'
+        ></img>
+      </div>
     </div>
   );
 };
