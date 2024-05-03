176f2bb0110645bc2e63efbfc75c52be1ccb9405
jennyjoo <100cllr@gmail.com>
2024-05-03 15:37:56 +0900

    feat : 결제 setTimeout 설정

--- src/components/view/OrderSubmit.tsx
+++ src/components/view/OrderSubmit.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface SuccessOrderResDto {
   userPoint: number;
@@ -6,6 +7,7 @@ interface SuccessOrderResDto {
 }
 
 const OrderSubmit = () => {
+  const navigation = useNavigate();
   const [data, setData] = useState<SuccessOrderResDto | null>(null);
   const [userId, setUserId] = useState<number | null>(null);
   const [orderListId, setOrderListId] = useState<number | null>(null);
@@ -38,20 +40,45 @@ const OrderSubmit = () => {
       });
   };
 
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      navigation('/');
+    }, 7000);
+  }, []);
 
   return (
-    <div>
+    <div className='flex flex-col max-w-screen-sm sm min-h-screen  justify-center items-center mx-auto p-20'>
       <img
         src='https://i.namu.wiki/i/pYdtlj-fQNTInL7V6QVzRNAB_9Ip74fCiowUbuIEb03dzfy9olYTgA6-SLOD4GRe-Uub5_zWT-hocLHOcJDBxw.svg'
         alt='logo'
+        className='size-64 m-10'
       ></img>
-      <h1>고객님의 주문번호는 {data.orderListId}번입니다.</h1>
+      {!data ? (
+        ''
+      ) : (
+        <h1>
+          고객님의 주문번호는{' '}
+          <strong className='text-mcred text-xl'>{data.orderListId}</strong>번
+          입니다.
+        </h1>
+      )}
       <h1>감사합니다.</h1>
       <br />
-      <h1>회원님의 적립금은 {data.userPoint}원입니다.</h1>
+      {!data ? (
+        ''
+      ) : (
+        <h1>
+          회원님의 적립금은 <span className='underline'>{data.userPoint}</span>
+          원입니다.
+        </h1>
+      )}
+      <br></br>
+      <button
+        className='text-xl font-bold bg-mcred rounded-lg px-7 text-white'
+        onClick={() => navigation('/')}
+      >
+        Home
+      </button>
     </div>
   );
 };
